7bf64e2b23802acae30377c6e19c32417488d7ff
Crecker2 <joshua.fayer@gmail.com>
2019-02-08 00:09:27 -0500

    added fixed navbar

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,11 +1,27 @@
-(function() {
-	  var links = $(".articlebody p a");
-	  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-	    // can also be
-	    //  links[i].hostname != 'subdomain.example.com'
-	    if (links[i].hostname != window.location.hostname) {
-	      links[i].target = '_blank';
-	      links[i].className += ' externalLink';
-	    }
-	  }
-	})();
+$(document).ready(function(){
+	(function() {
+		  var links = $(".articlebody p a");
+		  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+		    // can also be
+		    //  links[i].hostname != 'subdomain.example.com'
+		    if (links[i].hostname != window.location.hostname) {
+		      links[i].target = '_blank';
+		      links[i].className += ' externalLink';
+		    }
+		  }
+		})();
+		var scrolled = false;
+		$(window).scroll(function(){
+			if(!scrolled){
+				scrolled = true;
+				if($(window).scrollTop() >= 100){
+		            $('.navbar-brand').css('font-size','20px');
+					$('nav').css('height','70px');
+		        } else {
+		            $('.navbar-brand').css('font-size','25px');
+					$('nav').css('height','75px');
+		        }
+				scrolled = false;
+			}
+		});
+});
